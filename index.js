4bdb24c30933fbe510f2236a51f0250ca3fe5c8b
Oren Saldanha <oren@akto.io>
2023-03-28 15:06:24 +0530

    My first action is ready

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const axios = require("axios")
 async function run() {
   let AKTO_START_TEST_ENDPOINT = ""
 
-  const AKTO_DASHBOARD_URL = core.getInput('AKTO_DASHBOARD_URL');
-  const AKTO_API_KEY = core.getInput('AKTO_API_KEY');
-  const AKTO_TEST_CONFIGURATION = core.getInput('AKTO_TEST_CONFIGURATION')
+  const AKTO_DASHBOARD_URL = core.getInput('AKTO_DASHBOARD_URL')
+  const AKTO_API_KEY = core.getInput('AKTO_API_KEY')
+  const AKTO_TEST_ID = core.getInput('AKTO_TEST_ID')
 
   if (AKTO_DASHBOARD_URL.endsWith("/")) {
     AKTO_START_TEST_ENDPOINT = AKTO_DASHBOARD_URL + "api/startTest"
@@ -18,6 +18,20 @@ async function run() {
   const GITHUB_REF_NAME = process.env.GITHUB_REF_NAME
   const GITHUB_SHA = process.env.GITHUB_SHA
 
+  console.log(GITHUB_REPOSITORY, GITHUB_REF_NAME, GITHUB_SHA)
+
+  // const data = {
+  //   "testingRunHexId": AKTO_TEST_ID,
+  //   "metadata": {
+  //     "platform": "Github Actions",
+  //     "repository": process.env.GITHUB_REPOSITORY,
+  //     "branch": process.env.GITHUB_REF_NAME,
+  //     "commit_sha": process.env.GITHUB_SHA
+  //   }
+  // }
+
+  
+
   const config = {
     method: 'post',
     url: AKTO_START_TEST_ENDPOINT,
@@ -26,7 +40,7 @@ async function run() {
       'X-API-KEY': AKTO_API_KEY,
       'account': 1000000
     },
-    data: AKTO_TEST_CONFIGURATION
+    data: data
   }
 
   try {
