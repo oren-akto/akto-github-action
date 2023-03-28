486cc6606a8b5866d4d2978f2008fc49b04a4362
Oren Saldanha <oren@akto.io>
2023-03-28 14:01:29 +0530

    update

--- index.js
+++ index.js
@@ -2,24 +2,36 @@ const core = require('@actions/core');
 const axios = require("axios")
 
 async function run() {
-  try {
-    const AKTO_DASHBOARD_URL = core.getInput('AKTO_DASHBOARD_URL');
-    const AKTO_ACCESS_TOKEN = core.getInput('AKTO_API_TOKEN');
-    const AKTO_TEST_CONFIGURATION = JSON.parse(core.getInput('AKTO_TEST_CONFIGURATION'));
+  let AKTO_START_TEST_ENDPOINT = ""
 
-    await axios({
-      url: AKTO_DASHBOARD_URL + '/api/startTest',
-      method: 'post',
-      headers: {'access-token': AKTO_ACCESS_TOKEN},
-      data: AKTO_TEST_CONFIGURATION
-    })
-    console.log("Akto API test started")
+  const AKTO_DASHBOARD_URL = core.getInput('AKTO_DASHBOARD_URL');
+  const AKTO_API_KEY = core.getInput('AKTO_API_KEY');
+  const AKTO_TEST_CONFIGURATION = JSON.parse(core.getInput('AKTO_TEST_CONFIGURATION'));
+
+  if (AKTO_DASHBOARD_URL.endsWith("/")) {
+    AKTO_START_TEST_ENDPOINT = AKTO_DASHBOARD_URL + "api/startTest"
+  } else {
+    AKTO_START_TEST_ENDPOINT = AKTO_DASHBOARD_URL + "/api/startTest"
+  }
+
+  const config = {
+    method: 'post',
+    url: AKTO_START_TEST_ENDPOINT,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': AKTO_API_KEY,
+      'account': 1000000
+    },
+    data: AKTO_TEST_CONFIGURATION
+  }
+
+  try {
+    await axios(config)
+    console.log("Akto CI/CD test started")
   } catch (error) {
     core.setFailed(error.message);
   }
-  console.log(process.env.GITHUB_REF)
-  console.log(process.env.GITHUB_SHA)
-  console.log(process.env.GITHUB_REPOSITORY)
+
 }
 
 run();
